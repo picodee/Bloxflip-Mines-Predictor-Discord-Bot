19c492969e1ec48cfb14c961b373d3d5541f44dc
picodee <125156335+picodee@users.noreply.github.com>
2023-10-26 19:46:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -49,4 +49,4 @@ async def mines(interaction: discord.Interaction, tile_amt: int, round_id : str)
         em.add_field(name='Error', value="Invalid round id")
         await interaction.response.send_message(embed=em)
 
-client.run('bot token')
+client.run('MTE2NzI0NjkzODQ2NzQ2NzMwNA.GaBMG6.ycXM7hGxCqkYJuFyX2oT5NU_UcKM5Sk11qh_SY')
